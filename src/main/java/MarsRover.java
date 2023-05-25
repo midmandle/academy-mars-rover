ae6b0b41821591ac0b536c66b76e41e697b2d14f
Rebeca Gonzalez <bequipedia@gmail.com>
2023-05-25 11:11:12 +0200

    refactor to extract functionality

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,19 +1,34 @@
 public class MarsRover {
 
     public static final String TURN_RIGHT = "R";
+    public static final String TURN_LEFT = "L";
 
     public String run(String commands) {
         String coordinates = "0:0:";
         String direction = "N";
-        if(commands.equals("L")){
+        if(commands.contains(TURN_LEFT)) {
+            direction = turnLeft(commands, direction);
+        }
+        if(commands.contains(TURN_RIGHT)) {
+            direction = turnRight(commands, direction);
+        }
+
+        return coordinates + direction;
+    }
+    private static String turnLeft(String commands, String direction) {
+        if (commands.equals(TURN_LEFT)) {
             direction = "W";
         }
-        if(commands.equals("LL")) {
+        if (commands.equals(TURN_LEFT + TURN_LEFT)) {
             direction = "S";
         }
-        if(commands.equals("LLL")) {
+        if (commands.equals(TURN_LEFT + TURN_LEFT + TURN_LEFT)) {
             direction = "E";
         }
+        return direction;
+    }
+
+    private static String turnRight(String commands, String direction) {
         if (commands.equals(TURN_RIGHT)) {
             direction = "E";
         }
@@ -23,6 +38,6 @@ public class MarsRover {
         if (commands.equals(TURN_RIGHT + TURN_RIGHT + TURN_RIGHT)) {
             direction = "W";
         }
-        return coordinates + direction;
+        return direction;
     }
 }
